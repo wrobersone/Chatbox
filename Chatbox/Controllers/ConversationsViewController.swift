2e185e9b9eb95b0f9bfc51b14334554444cb8df9
William Robersone <william.robersone@gmail.com>
2020-06-28 22:44:17 -0700

    Getting Started

---
+++ Chatbox/Controllers/ConversationsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Chatbox
+//
+//  Created by William Robersone on 6/28/20.
+//  Copyright © 2020 William Robersone. All rights reserved.
+//
+
+import UIKit
+
+class ConversationsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
